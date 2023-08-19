f5fc2fdf7a021dafa231fafc4390145eb7475c2f
nickaldwin <nickalemoncito98@gmail.com>
2023-08-19 15:12:25 +0800

    creating main.jsx

---
+++ views/main.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Head from "./components/Head";
+
+const Main =({ children }) => (
+    <>
+        <Main/>
+        <body>{children}</body>
+    </>
+);
+
+export default Main;
